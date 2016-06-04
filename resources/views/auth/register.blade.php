7646e10bfffdbaf692bb99438bc7775d74394db7
George Miller <git@duffleman.co.uk>
2016-06-04 01:43:33 +0100

    Registration now gives detailed errors

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,24 +9,44 @@
             <div class="input-field col s12">
                 <input id="name" name="name" type="text" class="validate" value="{{ old('name') }}">
                 <label for="name">Name</label>
+                @if ($errors->has('name'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('name') }}</strong>
+                    </span>
+                @endif
             </div>
         </div>
         <div class="row">
             <div class="input-field col s12">
                 <input id="email" name="email" type="email" class="validate" value="{{ old('email') }}">
                 <label for="email">Email</label>
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
             </div>
         </div>
         <div class="row">
             <div class="input-field col s12">
                 <input id="password" name="password" type="password" class="validate">
                 <label for="password">Password</label>
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
             </div>
         </div>
         <div class="row">
             <div class="input-field col s12">
                 <input id="password_confirmation" name="password_confirmation" type="password" class="validate">
                 <label for="password_confirmation">Confirm Password</label>
+                @if ($errors->has('password_confirmation'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                    </span>
+                @endif
             </div>
         </div>
         <div class="row">
