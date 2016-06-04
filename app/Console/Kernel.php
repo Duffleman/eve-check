aa157602adf06116415049451e766a86fa60b975
George Miller <git@duffleman.co.uk>
2016-06-04 01:26:11 +0100

    Every 30 minutes top :P

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,6 +23,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('check')->everyMinute();
+        $schedule->command('check')->everyThirtyMinutes();
     }
 }
